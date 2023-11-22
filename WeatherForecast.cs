95d976de7d8bb2846a9d83c34587dea64b511835
RolyPoly-Rampage <rolypolyrampage@gmail.com>
2023-11-22 17:07:15 -0600

    first commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace broke_as_a_mf;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
